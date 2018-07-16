9907e3a453a7d975c0692bb10a227c5d34403159
Aisha Coles <coles_aisha@yahoo.com>
2018-07-16 15:32:06 -0700

    Add shuffle array function

--- js/designs.js
+++ js/designs.js
@@ -1,1 +1,15 @@
 //Javascript for Memory Card Game
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
